76a52b487389d6a0b5041f7f4556d9c1e00c3427
Thibault Harel <thibault.harel1@gmail.com>
2024-04-21 23:17:01 +0200

    tooltips

--- src/app/features/games/hanabi/components/hanabi-state/hanabi-state.component.ts
+++ src/app/features/games/hanabi/components/hanabi-state/hanabi-state.component.ts
@@ -2,11 +2,12 @@ import {ChangeDetectionStrategy, Component, Input} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {HanabiGame} from "../../models/hanabi-game.model";
 import {MatIcon} from "@angular/material/icon";
+import {MatTooltip} from "@angular/material/tooltip";
 
 @Component({
   selector: 'app-hanabi-state',
   standalone: true,
-  imports: [CommonModule, MatIcon],
+  imports: [CommonModule, MatIcon, MatTooltip],
   templateUrl: './hanabi-state.component.html',
   styleUrls: ['./hanabi-state.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
