7ec45b8b66a8210de7ed9a74266301041ce9b86f
Thibault Harel <thibault.harel1@gmail.com>
2024-05-30 10:47:14 +0200

    improving light theme

--- src/app/features/games/hanabi/components/hanabi-infos/hanabi-infos.component.ts
+++ src/app/features/games/hanabi/components/hanabi-infos/hanabi-infos.component.ts
@@ -4,11 +4,12 @@ import {MatIcon} from "@angular/material/icon";
 import {MatTooltip} from "@angular/material/tooltip";
 import {HanabiGame} from "../../models/hanabi-game.model";
 import {Changes} from "../../../../../core/utils/changes.model";
+import {MatCard} from "@angular/material/card";
 
 @Component({
   selector: 'app-hanabi-infos',
   standalone: true,
-  imports: [CommonModule, MatIcon, MatTooltip],
+  imports: [CommonModule, MatIcon, MatTooltip, MatCard],
   templateUrl: './hanabi-infos.component.html',
   styleUrls: ['./hanabi-infos.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
